59932452f2afe8d12126f7e854026473c83d6861
lyubchenko-ivan <lubchenko2044@gmail.com>
2020-12-10 08:25:22 +0300

    v2.0

--- js/script.js
+++ js/script.js
@@ -2,19 +2,35 @@ let point = function (elem){
     elem.style.backgroundColor = 'red';
 }
 
-let container = document.getElementById('container');
 
-for (let i = 0; i < 16; i++) {
-    container.innerHTML += `<div class = 'row'></div>`
+let reset = function (){
+    start();
 }
 
-let rows = document.getElementsByClassName('row');
 
-for (let i = 0; i < 16; i++) {
-    let temp = rows[i];
+
+let start = function (){
+    let container = document.getElementById('container');
+
+    container.innerHTML = '<input type="button" value="Сбросить" onclick="reset()"> <input type = "text" placeholder = "какое количество квадратов?" id = "count"><br>';
+
+    alert(document.getElementById("count").value);
+    // alert(count);
+    for (let i = 0; i < 16; i++) {
+        container.innerHTML += `<div class = 'row'></div>`
+    }
     
-    for (let j = 0; j < 16; j++) {
-        temp.innerHTML += `<div class='col' onmouseover='point(this)'></div>` 
+    let rows = document.getElementsByClassName('row');
+    
+    for (let i = 0; i < 16; i++) {
+        let temp = rows[i];
         
+        for (let j = 0; j < 16; j++) {
+            temp.innerHTML += `<div class='col' onmouseover='point(this)'></div>` 
+            
+        }
     }
 }
+
+
+start();
