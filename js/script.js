9bb9f3544526538156e1a938857957eaf494b2f6
lyubchenko-ivan <lubchenko2044@gmail.com>
2020-12-11 00:03:28 +0300

    release drawer

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
-let point = function (elem){
+
+let tored = function(elem){
     elem.style.backgroundColor = 'red';
 }
 
+let getSquare = function (count){
+    //console.log(count);
 
-let reset = function (){
-    start();
-}
-
-
-
-let start = function (){
     let container = document.getElementById('container');
+    container.innerHTML = '';
 
-    container.innerHTML = '<input type="button" value="Сбросить" onclick="reset()"> <input type = "text" placeholder = "какое количество квадратов?" id = "count"><br>';
-
-    alert(document.getElementById("count").value);
-    // alert(count);
-    for (let i = 0; i < 16; i++) {
-        container.innerHTML += `<div class = 'row'></div>`
-    }
-    
-    let rows = document.getElementsByClassName('row');
-    
-    for (let i = 0; i < 16; i++) {
-        let temp = rows[i];
-        
-        for (let j = 0; j < 16; j++) {
-            temp.innerHTML += `<div class='col' onmouseover='point(this)'></div>` 
-            
+    for (let i = 0; i < count; i++){
+        container.innerHTML += `<div class='rows'></div>`;
+        let rows = document.getElementsByClassName('rows');
+        rows[i].style.height = `${90 / count}vh`;
+        for (let j = 0; j < count; j++){
+            rows[i].innerHTML += `<div class='cols' onmouseover="tored(this)"></div>`;
+            // let col = document.getElementById(`${i}${j}`);
+            let cols = document.getElementsByClassName('cols');
+            for (let k = 0; k <cols.length; k++){    
+                cols[k].style.width = `${99 / count}%`;
+            }    
         }
     }
+
+    
+
+}
+
+let access = function(){
+    let text = document.getElementById('count');
+    let count = text.value;
+    text.value = '';
+
+    getSquare(count);
 }
 
 
-start();
